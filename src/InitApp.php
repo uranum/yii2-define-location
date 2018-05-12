277bb4d5d37e19eb6cb9d13729a60b09e8ee72f0
Eugeny_Emelyanov <e9139905539@gmail.com>
2018-05-12 12:53:20 +0700

    fix init session

--- src/InitApp.php
+++ src/InitApp.php
@@ -22,11 +22,15 @@ class InitApp implements BootstrapInterface
         $container = \Yii::$container;
 
         if ($app instanceof \yii\web\Application) {
+            $session = $app->session;
             $app->user->on(User::EVENT_AFTER_LOGIN, [$container->get('uranum\location\components\LocationSetter'), 'handleLoginEvent']);
-            $city = $app->session->get(Module::USER_CITY);
-            $app->user->on(User::EVENT_AFTER_LOGOUT, function () use ($city, $app) {
-                $app->session->set(Module::USER_CITY, $city);
-            });
+
+            if ($session->isActive) {
+                $city = $session->get(Module::USER_CITY);
+                $app->user->on(User::EVENT_AFTER_LOGOUT, function () use ($city, $session) {
+                    $session->set(Module::USER_CITY, $city);
+                });
+            }
 
             $container->setSingleton('LocationModule', function() use ($app) {
                 $module = Module::getInstance();
